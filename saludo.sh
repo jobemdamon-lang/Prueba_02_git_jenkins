d33242cdfedb640d49fe3fdc238414771cccd734
Jose <josdavbm10@gmail.com>
2026-02-13 21:42:05 -0500

    Trigger with webhook desde git and usando ngrok(puente red iinterna)

--- saludo.sh
+++ saludo.sh
@@ -1,3 +1,3 @@
 echo "Hola mundo con Jenkins y Github"
 echo "Probando trigger desde Github y Jenkins"
-echo "Probando trigger con webhook desde github y usando grok *~*"
+echo "Probando trigger con webhook desde github y usando ngrok para puentear la red interna  "
