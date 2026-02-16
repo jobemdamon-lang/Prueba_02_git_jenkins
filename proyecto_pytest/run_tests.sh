e3eff73cacf9222959662927d26f1a1afafbd087
Jose <josdavbm10@gmail.com>
2026-02-16 18:16:34 -0500

    run_tests.sh was modified, now

--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -1,37 +1,31 @@
 #!/bin/bash
 
-
 echo "Iniciando ejecución de pruebas en Jenkins ..."
 
-# Verificar si el entorno virtual existe
+# Crear entorno virtual si no existe
 if [ ! -d "venv" ]; then 
-    echo " Entorno virtual no encontrado. Creándolo ..."
+    echo "Entorno virtual no encontrado. Creándolo ..."
     python3 -m venv venv
 fi
 
-# Activar el entorno virtual correctamente
-
+# Activar entorno virtual
 if [ -f "venv/bin/activate" ]; then 
     source venv/bin/activate
-elif [ -f "venv/bin/activate" ]; then # Para linux
-    source venv/bin/activate
 else 
-    echo " Error: No se pudo activar el entorno virtual."
+    echo "Error: No se pudo activar el entorno virtual."
     exit 1
 fi
 
-
-
-# Verificamos si 'pip' esta instalado correctamente
-echo "instalando dependencias ..."
-pip install --upgrade pip --break-system-packages
-pip install -r requirements.txt --break-system-packages
-
+echo "Instalando dependencias ..."
+pip install --upgrade pip
+pip install -r requirements.txt
 
 mkdir -p reports
 
-# Ejecutar las pruebas
-echo "ejecutando pruebas con pytest"
-venv/bin/python pytest test/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
+echo "Ejecutando pruebas con pytest ..."
+pytest test/ \
+--junitxml=reports/test-results.xml \
+--html=reports/test-results.html \
+--self-contained-html
 
-echo "pruebas finalizadas. resultados en reports/"
+echo "Pruebas finalizadas. Resultados en reports/"
